4ae564944de81f5bc3c864de87e8c5f845af5dec
CrazyLegumes <deepwns@live.com>
2016-12-05 14:20:28 -0600

    Main Menu
    Basic Main MEnu things are done. Fade in Text

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MainMenuUI : MonoBehaviour {
+    [SerializeField]
+    private Text Title;
+    [SerializeField]
+    private Text StartGame;
+    [SerializeField]
+    private Text Exit;
+
+
+
+    void Awake()
+    {
+        Title.text = "A Vagrants Cry";
+        StartGame.text = "Start";
+        Exit.text = "Exit";
+        Title.color = Color.clear;
+        StartGame.color = Color.clear;
+        Exit.color = Color.clear;
+    }
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(FadeInTitle());
+	}
+
+    IEnumerator FadeInTitle()
+    {
+        Title.DOColor(Color.white, 4);
+        yield return new WaitForSeconds(2);
+        StartGame.DOColor(Color.white, 3);
+        yield return new WaitForSeconds(.5f);
+        Exit.DOColor(Color.white, 3);
+
+        yield break;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
