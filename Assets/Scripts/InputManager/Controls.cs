39edde592f3903d08a560bc32468004a885b01bb
CrazyLegumes <deepwns@live.com>
2016-12-05 11:56:00 -0600

    Movement
    Added basic movement
    Added Jumping Mechanic

--- Assets/Scripts/InputManager/Controls.cs
+++ Assets/Scripts/InputManager/Controls.cs
@@ -4,19 +4,74 @@ using System.Collections;
 
 
 
-public class Controls : MonoBehaviour {
+public class Controls : MonoBehaviour
+{
 
     private CharacterController body;
     private bool canmove;
-   
+    private Vector3 inp = Vector3.zero;
+    private const float gravity = 30f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    [SerializeField]
+    private float movespeed;
+
+    [SerializeField]
+    private float jumpheight;
+
+    // Use this for initialization
+    void Start()
+    {
+        canmove = true;
+        body = GetComponent<CharacterController>();
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+
+
+    }
+
+
+    private void Movement()
+    {
+        if (canmove)
+        {
+            
+            inp.x = Inputs.HorizontalAxis();
+            inp.z = Inputs.VerticalAxis();
+            inp.x *= movespeed;
+            inp.z *= movespeed;
+
+            if (body.isGrounded)
+            {
+                inp.x = Inputs.HorizontalAxis();
+                inp.z = Inputs.VerticalAxis();
+
+
+                inp = transform.TransformDirection(inp);
+
+                inp.x *= movespeed;
+                inp.z *= movespeed;
+
+
+                if (Inputs.B_Button())
+                {
+                    inp.y = jumpheight;
+                }
+            }
+
+            inp.y -= gravity * Time.deltaTime;
+            body.Move(inp * Time.deltaTime);
+        }
+    }
+
 }
+
