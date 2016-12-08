52f891889769e9304c225ad6a8b1b3c054bbd22b
CrazyLegumes <deepwns@live.com>
2016-12-07 18:43:42 -0600

    Hold attack
    Hold Attack has been added

---
+++ Assets/Scripts/PlayerStuff/Skills/GenericHoldAttack.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericHoldAttack : Skills {
+
+    public override void Init()
+    {
+        base.Init();
+        ID = 1003;
+        Cost = 5;
+        Type = attacktype.Hold;
+        Desc = "A generic charged attack! Hold the indicated key until you see ! for maximum damage";
+        Timer = 2;
+    }
+}
